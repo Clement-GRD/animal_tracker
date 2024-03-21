0a9e83f0318459447eb8b6efbe59e6f317b73903
Acyyd <ptr.testus@gmail.com>
2024-03-21 15:19:53 -0400

    Added model and prediction.

--- src/API.py
+++ src/API.py
@@ -2,24 +2,24 @@
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
 from utils.preprocess_image import read_image_from_buffer, preprocess_image
-from tensorflow.keras.
-# import matplotlib.pyplot as plt
+from utils.predictions import predict
+from tensorflow.keras.models import load_model
 
 app = FastAPI()
 
-model = load_model('../models/model/pretrained_fine_300_ratio')
+model = load_model("models/model/pretrained_fine_300_ratio")
 
 @app.get("/ping")
 async def ping():
     return "Server running"
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
+async def predict_req(file: UploadFile = File(...)):
     image =  read_image_from_buffer(await file.read())
     preprocessed_image =  preprocess_image(image)
-    # plt.imshow(preprocessed_image[0])
+    predicted_class, likelihood = predict(preprocessed_image, model)
 
-    return image.shape
+    return {"predicted_class": predicted_class, "likelihood": likelihood}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
